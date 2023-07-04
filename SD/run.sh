2386189e6fe132d399b7ad9156116deb6dc95e23
wuzhiping <wuzhiping@users.noreply.github.com>
2023-07-05 06:41:26 +0800

    Update run.sh

--- SD/run.sh
+++ SD/run.sh
@@ -1,6 +1,8 @@
 docker run \
 --rm -it --gpus=all \
 -v $PWD/v1-5-pruned-emaonly.safetensors:/stable-diffusion-webui/models/Stable-diffusion/v1-5-pruned-emaonly.safetensors \
+-v $PWD/launch_utils.py:stable-diffusion-webui/modules/launch_utils.py \
 -p 7860:7860 \
 shawoo/cuda:SD \
-./webui.sh
+python launch.py --listen --no-half
+#./webui.sh
