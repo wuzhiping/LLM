4c31acb461c5de9f6319bad4ff92c6bf84babc84
wuzhiping <wuzhiping@users.noreply.github.com>
2023-07-05 13:41:48 +0800

    Add files via upload

--- WD/run.sh
+++ WD/run.sh
@@ -1,8 +1,9 @@
 docker run \
 --rm -it --gpus=all \
--v $PWD/../LLM/models:/wenda/model/chatglm2-6b \
--v $PWD/m3e-base:/wenda/model/m3e-base
--v $PQD/config.yml:/wenda/config.yml
+-v $PWD/../LLM/models:/wenda/model/chatglm26b \
+-v $PWD/m3e-base:/wenda/model/m3e-base \
+-v $PWD/config.yml:/wenda/config.yml \
+-v $PWD/txt/:/wenda/txt/ \
 -p 7860:7860 \
 shawoo/cuda:WD \
 python wenda.py -t glm6b
